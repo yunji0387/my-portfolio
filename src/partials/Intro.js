f90a66b49d42fba4eb80799765802d046ee51eb9
yunji0387 <yunji0387@gmail.com>
2023-07-17 16:46:54 -0500

    add responsive design for mobile devices

--- src/partials/Intro.js
+++ src/partials/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
         <div className="intro">
             <div className="intro-section">
                 <p className="intro-text">
-                    I am a recent Computer Science graduates with a strong passion for coding and software development. While I may not have prior professional experience in the field, I am highly motivated to pursue a career in coding, willing to learn new skills, and contribute to a dynamic team.
+                    I am a recent Computer Science graduates with a strong passion for coding and and a keen interest in web and software development. While I may not have prior professional experience in the field, I am highly motivated to pursue a career in coding, willing to learn new skills, and contribute to a dynamic team.
                 </p>
             </div>
         </div>
