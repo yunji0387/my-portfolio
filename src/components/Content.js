c545dea7e710df5b1a99330e759f538bc9555d49
yunji0387 <yunji0387@gmail.com>
2023-08-21 12:00:27 -0500

    Redesign About page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Content.css';
-import '../components/Band.css';
+// import React from 'react';
+// import './Content.css';
+// import '../components/Band.css';
 
-function Content(props) {
-  return (
-    <div className="content">
-      <div className="band content-section">
-        <p className="content-text">{props.text}</p>
-      </div>
-    </div>
-  );
-}
+// function Content(props) {
+//   return (
+//     <div className="content">
+//       <div className="band content-section">
+//         <p className="content-text">{props.text}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Content;
+// export default Content;
